8ca07867ee5c09f1f329b7c75fa0f04edddbbf96
Egor Kuznechyk <nayone.tapok@gmail.com>
2025-11-22 15:00:56 +0300

    fix: solving the problem with an incorrect ObjectID

--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -1,8 +1,8 @@
 import jwt from 'jsonwebtoken';
 import { JWT_SECRET_KEY } from '../config/config';
-import { ObjectId } from 'mongoose';
+import { Types } from 'mongoose';
 
-export const generateJwt = (_id: ObjectId, role: string): string => {
+export const generateJwt = (_id: Types.ObjectId, role: string): string => {
     const payload = {
         _id,
         role
